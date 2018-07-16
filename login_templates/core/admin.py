72802f7399ae02cd3742c753e9f9195a6c738bf8
Silvio Machado <silvinho485@users.noreply.github.com>
2018-07-16 14:25:54 -0300

    img field in core.html

--- login_templates/core/admin.py
+++ login_templates/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 from .models import Profile
-admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'age', 'pic')
+
+admin.site.register(Profile, ProfileAdmin)
+
+
